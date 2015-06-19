65ad1ee0742fa1743c9e08ab7b994ad35d12f5bb
Saatvik Shukla <me@saatvikshukla.com>
2015-06-19 13:43:55 +0530

    Add a Rainbow <3

--- script.sh
+++ script.sh
@@ -5,6 +5,22 @@
 echo -e '\0033\0143'
 clear
 
+
+# Our Rainbow
+red='tput setaf 1'              # red
+green='tput setaf 2'            # green
+yellow='tput setaf 3'           # yellow
+blue='tput setaf 4'             # blue
+violet='tput setaf 5'           # violet
+cyan='tput setaf 6'             # cyan
+white='tput setaf 7'            # white
+txtbld=$(tput bold)             # Bold
+bldred=${txtbld}$(tput setaf 1) # Bold red
+bldgrn=${txtbld}$(tput setaf 2) # Bold green
+bldblu=${txtbld}$(tput setaf 4) # Bold blue
+bldcya=${txtbld}$(tput setaf 6) # Bold cyan
+normal='tput sgr0'
+
 function io_main_splash () {
 	sleep 0.075
 	echo -e " .___        _____.__       .__  __  .__             ________    _________ "  
