c25e7762d37e99b8be61c53db999ddc6f5f155ca
Saatvik Shukla <me@saatvikshukla.com>
2015-06-23 17:22:22 +0530

    Fix issue with SETUP_BUILD's official tag selection.
    Thanks to @Langes72 for helping with that :)

--- build-io.sh
+++ build-io.sh
@@ -254,8 +254,8 @@ function CONFIGURE_BUILD_OPTIONS() {
 function SETUP_BUILD () {
 	if [[ -n $TARGET_PRODUCT ]]; then
 		echo -e "checking if official or not"
-		OFFICIAL_DEVICES="ariesve,I9082,kmini3g,s3ve3g,titan,falcon,condor,taoshan,yuga,honami,togari,leo,armani,cancro,tomato"
-		for OFFICIAL_DEVICE in $OFFICIAL_DEVICES; do
+		OFFICIAL_DEVICES=(ariesve I9082 kmini3g s3ve3g titan falcon condor taoshan yuga honami togari leo armani cancro tomato)
+		for OFFICIAL_DEVICE in ${OFFICIAL_DEVICES[@]}; do
 			if [[ ${TARGET_PRODUCT} == ${OFFICIAL_DEVICE} ]]; then
 				export IO_BUILDTYPE=OFFICIAL
 				echo -e "$TARGET_PRODUCT is an OFFICIAL InfinitiveOS device"
